b031c8053e95e8ba78bb22d56ef584280bd39b85
Sobrino Julian <sobrinojulian@protonmail.com>
2024-03-19 15:15:15 -0300

    tarea clase 1: contador

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,86 @@ void main() {
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!!!'),
+      home: const CounterScreen(),
+    );
+  }
+}
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({Key? key}) : super(key: key);
+
+  @override
+  _CounterScreenState createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      if (_counter < 10) {
+        _counter++;
+      }
+    });
+  }
+
+  void _decrement() {
+    setState(() {
+      if (_counter > 0) {
+        _counter--;
+      }
+    });
+  }
+
+  Color _getTextColor(int value) {
+    if (value >= 0 && value <= 3) {
+      return Colors.green;
+    } else if (value >= 4 && value <= 7) {
+      return Colors.yellow;
+    } else {
+      return Colors.red;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_counter',
+              style: TextStyle(
+                fontSize: 48,
+                color: _getTextColor(_counter),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _increment,
+                  child: const Icon(Icons.add),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: _decrement,
+                  child: const Icon(Icons.remove),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
